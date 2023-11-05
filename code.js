a65f8398693ea89be3abb11d22a423f547dcb819
Narli Tavahin <121438793+NarliTavahin@users.noreply.github.com>
2023-11-05 09:34:08 -0800

    upload

---
+++ code.js
@@ -0,0 +1,16 @@
+function Submit() {
+    let number = document.getElementById("num").value;
+    document.getElementById("out-put").value = OddOrEven(number);
+}
+
+function OddOrEven(number) {
+
+    if(number % 2 == 0) 
+        document.getElementById("out-put").value = number + " is an Even Number";
+
+    else 
+        document.getElementById("out-put").value = number + " is an Odd Number";
+        
+
+    return OddOrEven(number);
+}
